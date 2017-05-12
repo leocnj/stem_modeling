53e55b21fa10795e2c3ef3b8824523d104dcfe22
Chen, Lei <LChen@ETS.ORG>
2017-05-12 11:34:10 -0400

    Implemented the idea
    now shiny APP can show table row and image correctly.

--- app/drawing_model_demo/app.R
+++ app/drawing_model_demo/app.R
@@ -1,4 +1,5 @@
 #
+
 # This is a Shiny web application. You can run the application by clicking
 # the 'Run App' button above.
 #
@@ -9,42 +10,50 @@
 
 library(shiny)
 
-imgs <- c("CMS60000678_1_1.jpeg",
-          "CMS60000692_1_1.jpeg",
-          "CMS60000702_1_1.jpeg")
- 
+# load needed libs
+library(tidyverse)
+library(magrittr)
+# load
+imgs <- feats_score$FileName
+tbl <- head(feats_score[,1:5])
+
 # Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Test..."),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-     selectInput("id", 
-                 label = "Choose image file ID",
-                 choices = imgs),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         # plotOutput("distPlot")
-        imageOutput("imgout")
-      )
-  )
-)
+ui <- fluidPage(# Application title
+  titlePanel("Drawing Modelling Demo"),
+  
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    selectInput("id",
+                label = "Choose image file ID",
+                choices = imgs),
+    
+    # show feature score row
+    # show drawing image
+    mainPanel(# plotOutput("distPlot")
+      h2("Table row w/ features and human scores"),
+      tableOutput("onerow_intable"),
+      h2("Drawing image"),
+      imageOutput("imgout"))
+  ))
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
- 
-   output$imgout <- renderImage({
-    filename <- normalizePath(file.path('./img', input$id))
+  # based on input$id to find the row to render
+  output$onerow_intable <- renderTable({
+  onerow <- feats_score %>%
+    filter(FileName == input$id)
+  })
+  
+  # based on https://shiny.rstudio.com/articles/images.html
+  output$imgout <- renderImage({
+    filename <-
+      normalizePath(file.path('./img', paste0(input$id, '.jpeg')))
     # Return a list containing the filename and alt text
     list(src = filename,
          alt = paste("Image ", input$id))
-    }, deleteFile = FALSE)
-   
+  }, deleteFile = FALSE)
+  
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
-
